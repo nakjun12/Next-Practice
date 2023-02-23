77da1be31b8b18033bd156d57a1d7101ac14e6e7
nakjun <wns450@gmail.com>
2023-02-23 14:10:34 +0900

    [FE FIX]detail 추가

--- nextjs-intro/next.config.js
+++ nextjs-intro/next.config.js
@@ -1,6 +1,25 @@
+const API_KEY = "1932b9e2f02fd3f152a4611317a68e97";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/old-blog/:path*",
+        destination: "/new-sexy-blog/:path*",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- nextjs-intro/src/pages/index.tsx
+++ nextjs-intro/src/pages/index.tsx
@@ -1,33 +1,69 @@
 import Seo from "@/components/Seo";
-import { useEffect, useState } from "react";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 
-const API_KEY = "1932b9e2f02fd3f152a4611317a68e97";
-
-interface IMovie {
+interface IMovieProps {
   id: number;
+  backdrop_path: string;
   original_title: string;
-}
-export default function Home() {
-  const [movies, setMovies] = useState<IMovie[]>([]);
-  useEffect(() => {
-    (async () => {
-      const { results } = await (
-        await fetch(
-          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
-        )
-      ).json();
-      setMovies(results);
-    })();
-  }, []);
+  overview: string;
+  poster_path: string;
+  title: string;
+  vote_average: number;
+  genre_ids: [number];
+
+export default function Home({
+  results,
+}: InferGetServerSidePropsType<GetServerSideProps>) {
+  console.log(results);
+
   return (
     <div>
       <Seo title="Home" />
-      {!movies && <h4>Loading...</h4>}
-      {movies?.map((movie) => (
-        <div key={movie.id}>
+      {results?.map((movie: any) => (
+        <div className="movie" key={movie.id}>
           <h4>{movie.original_title}</h4>
         </div>
       ))}
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
     </div>
   );
+}
+
+export async function getServerSideProps({}: GetServerSideProps) {
+  const { results } = await (
+    await fetch(`http://localhost:3000/api/movies`)
+  ).json();
+
+  if (results) {
+    return {
+      props: {
+        results,
+      },
+    };
+  } else
+    return {
+      props: {
+        results: null,
+      },
+    };
 }
